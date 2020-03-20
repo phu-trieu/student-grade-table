31e59e6e9916e367f9b3b2d270badb54f19a9b4e
Phu <phutrieu95@gmail.com>
2020-03-20 15:50:32 -0700

    Created App class with methods and constructor and instantiated App class

---
+++ components/app.js
@@ -0,0 +1,26 @@
+class App {
+  constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+  handleGetGradesError(error) {
+    console.error(error);
+  }
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  }
+  getGrades() {
+    $.ajax({
+      method: 'GET',
+      url: 'https://sgt.lfzprototypes.com/api/grades',
+      headers: {
+        "X-Access-Token":"2TBfI97w"
+      },
+      success: this.handleGetGradesSuccess,
+      error: this.handleGetGradesError
+    })
+  }
+  start() {
+    this.getGrades();
+  }
+}
